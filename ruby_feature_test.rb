c1a4f014409930dcc4d3e31e43c71033e973a75a
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-10-02 16:24:15 +0100

    Updated feature test to include Print

--- ruby_feature_test.rb
+++ ruby_feature_test.rb
@@ -1,11 +1,29 @@
+require './lib/account.rb'
 require './lib/transactions.rb'
+require './lib/print.rb'
+
+
+
 
 transactions = Transactions.new
 
-transactions.deposit("12/10/2016", 100)
+account = Account.new
 
+p account.balance
+
+transactions.deposit("12/10/2017", 100)
+
+p transactions.account.balance
+
+p account.balance
+
+transactions.withdraw("15/10/2017", 50)
+
+p transactions.account.balance
 p transactions.all_transactions
 
-transactions.withdraw(50)
+p account.balance
 
-p transactions.all_transactions[0]
+print = Print.new(transactions.all_transactions)
+
+p print.print_balance
