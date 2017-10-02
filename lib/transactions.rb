ce59895736ac249a91e708256247d01ee560f379
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-10-02 12:23:51 +0100

    Added deposit method to transactions

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,8 +1,13 @@
 class Transactions
 
-  attr_reader :all_transactions
+  attr_reader :all_transactions, :balance
 
-  def initialize
+  def initialize(balance = nil)
     @all_transactions = []
+    @balance = 0
+  end
+
+  def deposit(amount)
+    @balance += amount
   end
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -7,4 +7,9 @@ describe Transactions do
     expect(subject.all_transactions).to eq([])
   end
 
+  it 'increases the balance when you deposit a sum' do
+    subject.deposit(100)
+    expect(subject.balance).to eq (100)
+  end
+
 end
