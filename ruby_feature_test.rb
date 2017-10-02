da8e853569fe748877aef77f771e7e49569665e0
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-10-02 17:30:19 +0100

    Feature test updated to include printing a statement

--- ruby_feature_test.rb
+++ ruby_feature_test.rb
@@ -19,11 +19,12 @@ p account.balance
 
 transactions.withdraw("15/10/2017", 50)
 
-p transactions.account.balance
-p transactions.all_transactions
-
 p account.balance
 
 print = Print.new(transactions.all_transactions)
 
+p print.format_statement
+
 p print.print_balance
+
+p print.balance_statement
