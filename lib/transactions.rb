c722374122e92092d04297d04346e31c4168c747
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-10-02 12:40:44 +0100

    Withdraw method added to transactions with error message

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -2,12 +2,17 @@ class Transactions
 
   attr_reader :all_transactions, :balance
 
-  def initialize(balance = nil)
+  def initialize
     @all_transactions = []
     @balance = 0
   end
 
   def deposit(amount)
     @balance += amount
+  end
+
+  def withdraw(amount)
+    raise 'Insufficient funds' if amount > @balance
+    @balance -= amount
   end
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -3,13 +3,26 @@ require 'transactions'
 describe Transactions do
   subject(:transactions) { described_class.new }
 
+  before(:each) do
+    subject.deposit(100)
+  end
+
   it 'initializes with an empty array' do
     expect(subject.all_transactions).to eq([])
   end
 
   it 'increases the balance when you deposit a sum' do
-    subject.deposit(100)
-    expect(subject.balance).to eq (100)
+
+    expect(subject.balance).to eq 100
+  end
+
+  it 'decreases the balance when you withdraw' do
+    subject.withdraw(50)
+    expect(subject.balance).to eq 50
+  end
+
+  it 'throws and error if you withdraw more than the balance' do
+    expect { subject.withdraw(110) }.to raise_error 'Insufficient funds'
   end
 
 end
