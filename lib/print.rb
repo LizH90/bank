a731c051c134bf83ab6af67a9479267db73c760e
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-10-02 17:29:43 +0100

    Print class updated to print out formatted statement

--- lib/print.rb
+++ lib/print.rb
@@ -1,20 +1,25 @@
 class Print
 
-  attr_reader :all_transactions
+  attr_reader :all_transactions, :balance_statement
 
   def initialize(all_transactions)
     @all_transactions = all_transactions
+    @balance_statement = ""
+  end
+
+  def format_statement
+    @all_transactions.each do |date,type,amount,balance|
+      if type == "credit"
+        @balance_statement += "#{date} || #{"%.2f" %amount} || || #{"%.2f" %balance}\n"
+      else
+        @balance_statement += " #{date} || || #{"%.2f" %amount} || #{"%.2f" %balance}\n"
+      end
+    end
   end
 
   def print_balance
-    string = "date || credit || debit || balance"
-    balance_statement = @all_transactions.each do |date,type,amount,balance|
-      if type == "credit"
-        string += " #{date} || #{"%.2f" %amount} || || #{"%.2f" %balance}"
-      else
-        string += " #{date} || || #{"%.2f" %amount} || #{"%.2f" %balance}"
-      end
-    end
-    string
+    puts "date || credit || debit || balance\n"
+    puts @balance_statement
   end
+
 end
--- spec/print_spec.rb
+++ spec/print_spec.rb
@@ -3,16 +3,20 @@ require 'print'
 describe Print do
   subject(:print) { described_class.new([["12/10/2017", "credit", 100, 100],["15/10/2017", "debit", 50, 50]]) }
 
-  it 'has print balance method' do
-    expect(subject.print_balance).to include("date || credit || debit || balance")
+  before(:each) do
+    subject.format_statement
   end
 
   it 'credits are put in the correct column' do
-    expect(subject.print_balance).to include("12/10/2017 || 100.00 || || 100.00")
+    expect(subject.balance_statement).to include("12/10/2017 || 100.00 || || 100.00")
   end
 
   it 'debits are put in the correct column' do
-    expect(subject.print_balance).to include("15/10/2017 || || 50.00 || 50.00")
+    expect(subject.balance_statement).to include("15/10/2017 || || 50.00 || 50.00")
+  end
+
+  it 'has print balance method' do
+    expect{subject.print_balance}.to output.to_stdout
   end
 
 end
