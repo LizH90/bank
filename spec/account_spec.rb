043d7f5eef534b85de4f753c0dd1d0e8ff8d24ee
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-10-02 11:50:02 +0100

    added first class account

---
+++ spec/account_spec.rb
@@ -0,0 +1,10 @@
+require 'account'
+
+describe Account do
+  subject(:account) {described_class.new}
+
+
+  it 'initializes with a default balance of zero' do
+    expect(subject.balance).to eq(0)
+  end
+end
